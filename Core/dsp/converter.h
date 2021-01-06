ab9b3ae68cf6b080fe731433cbe7b6ab81fbf64a
Howard Su <howard0su@gmail.com>
2021-01-06 20:56:47 +0800

    Add RAND support

--- Core/dsp/converter.h
+++ Core/dsp/converter.h
@@ -5,9 +5,24 @@
 class converter : public calculator<int16_t, float>
 {
 public:
-    converter(ringbuffer<int16_t> *adc) : calculator(adc)
+    converter(ringbuffer<int16_t> *adc) : calculator(adc), randADC(false)
     {
+        // load RAND table for ADC
+        for (int k = 0; k < 65536; k++)
+        {
+            if ((k & 1) == 1)
+            {
+                RandTable[k] = (k ^ 0xFFFE);
+            }
+            else
+            {
+                RandTable[k] = k;
+            }
+        }
     }
+
+    void setRand(bool val) { this->randADC = val; }
+    bool getRand() const { return this->randADC; }
 
 protected:
     void process() override
@@ -15,11 +30,22 @@ protected:
         auto *src = input->getReadPtr();
         auto *dest = output->getWritePtr();
 
-        // TODO: assume read/write has same block size
-        for (int i = 0; i < getInputBlockSize(); ++i)
-            dest[i] = float(src[i]);
+        if (!randADC)
+        {
+            for (int i = 0; i < getInputBlockSize(); ++i)
+                dest[i] = float(src[i]);
+        }
+        else
+        {
+            for (int i = 0; i < getInputBlockSize(); ++i)
+                dest[i] = float(RandTable[(uint16_t)src[i]]);
+        }
 
         output->WriteDone();
         input->ReadDone();
     }
+
+private:
+    bool randADC;       // randomized ADC output
+    int16_t RandTable[65536];  // ADC RANDomize table used to whitening EMI from ADC data bus.
 };
--- unittest/calculator_test.cpp
+++ unittest/calculator_test.cpp
@@ -46,7 +46,7 @@ TEST_CASE(ConverterFixture, TwoThreadsThreadTest)
     auto thread1 = std::thread([&input, count] {
         for (int i = 0; i < count; i++) {
             auto *ptr = input.getWritePtr();
-            memset(ptr, 0x5A, input.getBlockSize());
+            memset(ptr, 0x51, input.getBlockSize());
             input.WriteDone();
         }
     });
@@ -55,7 +55,41 @@ TEST_CASE(ConverterFixture, TwoThreadsThreadTest)
     auto thread2 = std::thread([&output, count, this] {
         for (int j = 0; j < count; j++) {
             auto *ptr = output->getReadPtr();
-            REQUIRE_EQUAL(*ptr, 23130.0f);
+            REQUIRE_EQUAL(*ptr, 20817.0f);
+            output->ReadDone();
+        }
+    });
+
+    thread2.join();
+    thread1.join();
+
+    conv.stop();
+    printf("Perf: %f\n", conv.getProcessTime());
+}
+
+TEST_CASE(ConverterFixture, TwoThreadsRANDThreadTest)
+{
+    auto input = ringbuffer<int16_t>(128);
+    auto conv = converter(&input);
+
+    input.setBlockSize(transferSize / 2);
+    conv.start();
+    conv.setRand(true);
+    const int count = 1280;
+
+    auto thread1 = std::thread([&input, count] {
+        for (int i = 0; i < count; i++) {
+            auto *ptr = input.getWritePtr();
+            memset(ptr, 0x51, input.getBlockSize());
+            input.WriteDone();
+        }
+    });
+
+    auto output= conv.getOutput();
+    auto thread2 = std::thread([&output, count, this] {
+        for (int j = 0; j < count; j++) {
+            auto *ptr = output->getReadPtr();
+            REQUIRE_EQUAL(*ptr, -20817.0f);
             output->ReadDone();
         }
     });
